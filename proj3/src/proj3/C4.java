108bcdf2a476edec84db3fedcb0f39e58ef9f10f
douadi <bourouaieh@yahoo.fr>
2022-09-23 08:56:19 +0100

    in the new branch

---
+++ proj3/src/proj3/C4.java
@@ -0,0 +1,14 @@
+package proj3;
+
+public class C4 {
+	//in the new
+	int k,j;
+
+	public C4(int k, int j) {
+		super();
+		this.k = k;
+		this.j = j;
+	}
+	
+
+}
--- proj3/src/proj3/T1.java
+++ proj3/src/proj3/T1.java
@@ -3,4 +3,6 @@ package proj3;
 public class T1 {
 int i;
 //comment
+//in the new branch 
+//no
 }
